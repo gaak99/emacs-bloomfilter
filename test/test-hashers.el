c17d49c2ce20d52911092adbd46fa96958453575
Glenn Barry <gaak99@gmail.com>
2016-09-08 12:56:01 -0400

    Tweak hash tests

--- test/test-hashers.el
+++ test/test-hashers.el
@@ -7,6 +7,7 @@
       '("LTBgjlDsR6" "2Js3OMjWw0" "eavUOuz7cn" "AOU7vNpMzT" "34sVSMHuFY" "VgqVATJZqm" "eFuflOQ3fz" "vGMWPfw9hW" "ywY9rfNvvx" "8KZJGaaL9y"))
 
 (defun test-hashers-3 (hfn l1 l2)
+"Quick sanity check on hash fn on range, collisions, etc."
   (-let* ((z1 (-zip-with (lambda (a b)
 			 (concat a "@" b)) l1 l2))
 	  (bv-size 1024)
@@ -15,14 +16,16 @@
 		    z1))
 	  (len1 (length h1))
 	  (h2 (-uniq h1))
-	  (len2 (length h2)))
-    (list len1 len2))); eyeball results -- todo automate&add ert support
+	  (len2 (length h2))
+	  (len12 (list len1 len2)); eq?
+	  (mm (list (-min h1) (-max h1)))); range
+   mm)) ; eyeball results -- todo automate&add ert support
 (test-hashers-3 'sxhash  rando-string-l1 rando-string-l2)
 (test-hashers-3 'sxhash  rando-string-l2 rando-string-l1)
 
 (test-hashers-3 (lambda (k)
-		  (bf--jenkins-hash k (length k)))
+		  (bfilter--jenkins-hash k (length k)))
 		rando-string-l2 rando-string-l1)
 (test-hashers-3 (lambda (k)
-		  (bf--jenkins-hash k (length k)))
+		  (bfilter--jenkins-hash k (length k)))
 		rando-string-l1 rando-string-l2)
